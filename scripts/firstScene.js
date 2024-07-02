ea1ad1f1af56ab94b2de1c54bbcb2720fe9f8559
Firas-Kobyaa <firaskobyaa@outlook.com>
2024-07-02 16:07:10 +0300

    fixed comments

--- scripts/firstScene.js
+++ scripts/firstScene.js
@@ -16,17 +16,17 @@ class FirstScene extends Phaser.Scene{
         
         // Set the desired height in pixels
         let map = this.add.image(960,540,'map')
-        // function to play the audio file and loop it
+        // increase width and height
         map.displayWidth = this.sys.game.config.width;; // Set the desired width in pixels
         map.displayHeight = this.sys.game.config.height; 
         
-        // func for playing music in the background and looping
-          var music = this.sound.add('music');
+        // function to play the audio file and loop it
+          const music = this.sound.add('music');
                 music.play({
                     loop: true
                 });
 
-            // increase width and height
+           
         
     }
     update (time,data){
