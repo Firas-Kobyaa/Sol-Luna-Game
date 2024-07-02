0b70d3f370561b6d7988eb234066a6f75da1f6ba
Firas-Kobyaa <firaskobyaa@outlook.com>
2024-07-02 16:04:09 +0300

    created a function for playing music in the backgrounf

--- scripts/firstScene.js
+++ scripts/firstScene.js
@@ -9,20 +9,24 @@ class FirstScene extends Phaser.Scene{
     
     preload (){
         this.load.image('map','images/space.png')
-        this.load.audio('Music', './audio/music.mp3');
+        this.load.audio('music', './audio/music.mp3');
     }
     create (data){
 
         
-
+        // Set the desired height in pixels
         let map = this.add.image(960,540,'map')
-      // Set the desired height in pixels
-      map.displayWidth = this.sys.game.config.width;; // Set the desired width in pixels
-      map.displayHeight = this.sys.game.config.height; 
-       
-       
+        // function to play the audio file and loop it
+        map.displayWidth = this.sys.game.config.width;; // Set the desired width in pixels
+        map.displayHeight = this.sys.game.config.height; 
+        
+        // func for playing music in the background and looping
+          var music = this.sound.add('music');
+                music.play({
+                    loop: true
+                });
 
-        // increase width and height
+            // increase width and height
         
     }
     update (time,data){
